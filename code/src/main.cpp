dc499bd072b74db13ac6d2eb85d913759f4eacfc
Seger01 <sjw.sars@student.avans.nl>
2024-09-03 14:49:23 +0200

    changed compile settings to allow for debugging

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
+  for (int i = 0; i < 10; i++) {
+
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    std::cout << "adlkfjasdflkj" << std::endl;
+  }
+  return 0;
 }
