5e4c495e8bf1c38071407aceb8d1027f419c88f0
Seger01 <sjw.sars@student.avans.nl>
2024-09-04 11:59:42 +0200

    changed cmake build process slightly

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
+#include "test.h"
+
 int main() {
   for (int i = 0; i < 10; i++) {
-
     std::cout << "Hello, World!" << std::endl;
-
-    std::cout << "adlkfjasdflkj" << std::endl;
   }
+
+  test();
   return 0;
 }
