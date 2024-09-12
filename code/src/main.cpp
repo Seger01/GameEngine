e9dc6f35300a780499ef781f0d8f72b44ab26f0c
Siempie30 <siemvermeulen@hotmail.com>
2024-09-12 19:39:46 +0200

    started working on sdl audio poc

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,6 +4,9 @@
 #include <chrono>
 #include <thread>
 #include <irrKlang.h>
+#include <SDL2/SDL.h>
+
+#include "sdl2.h"
 
 void ERRCHECK(FMOD_RESULT result) {
     if (result != FMOD_OK) {
@@ -128,7 +131,57 @@ char c;
     engine->drop();
 }
 
+void sdlMixer() {
+    using std::cerr;
+    using std::endl;
+
+        auto sys = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING);
+    if (!sys) {
+        cerr << "Error creating SDL2 system: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    auto win = sdl2::make_window("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+    if (!win) {
+        cerr << "Error creating window: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    auto ren
+        = sdl2::make_renderer(win.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!ren) {
+        cerr << "Error creating renderer: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    auto file = SDL_RWFromFile(IMGDIR "grumpy-cat.bmp", "rb");
+    if (file == nullptr) {
+        cerr << "Error reading file: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    auto bmp = sdl2::make_bmp(file);
+    if (!bmp) {
+        cerr << "Error creating surface: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    auto tex = sdl2::make_texture(ren.get(), bmp.get());
+    if (!tex) {
+        cerr << "Error creating texture: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < 20; i++) {
+        SDL_RenderClear(ren.get());
+        SDL_RenderCopy(ren.get(), tex.get(), nullptr, nullptr);
+        SDL_RenderPresent(ren.get());
+        SDL_Delay(100);
+    }
+}
+
 int main() {
     //fmod();
-    IrrKlang();
+    //IrrKlang();
+    sdlMixer();
 }
