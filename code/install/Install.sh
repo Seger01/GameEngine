d818eafbd20e577a772df66978608074f0aa6a08
heavydragon99 <heavydragon99@outlook.com>
2024-09-24 11:26:24 +0200

    Added script for SLikeNet download
    Script that can download SLikeNet

---
+++ code/install/Install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#call all install files
+./SDL2_install.sh
+./SLikeNet_install.sh
---
+++ code/install/SLikeNet_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#create slikenet dir and source dir
+cd ../external
+mkdir slikenet
+mkdir slikenet/source
+
+#download and unzip slikenet
+cd slikenet/source
+wget https://www.slikesoft.com/download/slikenet_0.1.3_source.tar.gz
+tar -xvf slikenet_0.1.3_source.tar.gz
+
+#create cmake dir
+cd ..
+mkdir cmake
+cd cmake
