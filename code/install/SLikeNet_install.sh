9a6d92923c3778cd4e23cab08c2b409595858d02
heavydragon99 <heavydragon99@outlook.com>
2024-09-24 12:07:00 +0200

    Download and compile SLikeNet

--- code/install/SLikeNet_install.sh
+++ code/install/SLikeNet_install.sh
@@ -2,15 +2,22 @@
 
 #create slikenet dir and source dir
 cd ../external
-mkdir slikenet
-mkdir slikenet/source
+# mkdir slikenet
+# mkdir slikenet/source
 
 #download and unzip slikenet
-cd slikenet/source
+# cd slikenet/source
 wget https://www.slikesoft.com/download/slikenet_0.1.3_source.tar.gz
-tar -xvf slikenet_0.1.3_source.tar.gz
+tar -xzvf slikenet_0.1.3_source.tar.gz
+rm slikenet_0.1.3_source.tar.gz
 
-#create cmake dir
-cd ..
-mkdir cmake
-cd cmake
+# #create cmake dir
+# cd ..
+# mkdir cmake
+# cd cmake
+
+# #call cmake
+# cmake ../source/slikenet
+
+# # #call make
+# # make -j 16
