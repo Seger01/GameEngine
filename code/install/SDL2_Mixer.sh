4ce60c18aff3a38b92408422f5d6c63fc23333d8
heavydragon99 <heavydragon99@outlook.com>
2024-09-25 10:52:27 +0200

    Added SDL and sdl_mixer
    Added install scripts and depencies for SDL and SDL_mixer

---
+++ code/install/SDL2_Mixer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+cd ../external
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to change directory to ../external."
+    exit 1
+fi
+
+# Check if the SDL_mixer directory already exists
+if [ -d "SDL_mixer" ]; then
+    echo "SDL_mixer repository already exists. Skipping clone."
+else
+    git clone https://github.com/libsdl-org/SDL_mixer.git --branch release-2.8.0
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to clone SDL_mixer repository."
+        exit 1
+    fi
+fi
+
+# # Install specific version of libsystemd0 if required
+# sudo apt-get install -y libsystemd0=249.11-0ubuntu3.7
+
+# # Check if the installation was successful
+# if [ $? -ne 0 ]; then
+#     echo "Failed to install libsystemd0 library"
+#     exit 1
+# fi
+
+# Install OpusFile library
+sudo apt-get install -y libopusfile-dev
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to install OpusFile library"
+    exit 1
+fi
+
+# Install libxmp library
+sudo apt-get install -y libxmp-dev
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to install libxmp library"
+    exit 1
+fi
+
+# Install wavpack library
+sudo apt-get install -y libwavpack-dev
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to install libwavpack-dev library"
+    exit 1
+fi
+
+# # Install FluidSynth library
+# sudo apt-get install -y libfluidsynth-dev
+
+# # Check if the installation was successful
+# if [ $? -ne 0 ]; then
+#     echo "Failed to install FluidSynth library"
+#     exit 1
+# fi
+
+echo "SDL2_mixer download completed successfully."
--- code/install/SLikeNet_install.sh
+++ code/install/SLikeNet_install.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-#create slikenet dir and source dir
 cd ../external
-# mkdir slikenet
-# mkdir slikenet/source
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to change directory to ../external."
+    exit 1
+fi
 
-#download and unzip slikenet
-# cd slikenet/source
-wget https://www.slikesoft.com/download/slikenet_0.1.3_source.tar.gz
-tar -xzvf slikenet_0.1.3_source.tar.gz
-rm slikenet_0.1.3_source.tar.gz
+# Check if the SLikeNet directory already exists
+if [ -d "SLikeNet" ]; then
+    echo "SLikeNet repository already exists. Skipping clone."
+else
+    git clone https://github.com/SLikeSoft/SLikeNet.git --branch v.0.1.3
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to clone SLikeNet repository."
+        exit 1
+    fi
+fi
 
-# #create cmake dir
-# cd ..
-# mkdir cmake
-# cd cmake
-
-# #call cmake
-# cmake ../source/slikenet
-
-# # #call make
-# # make -j 16
+echo "SLikeNet download completed successfully."
