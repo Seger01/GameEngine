a987e9bcc7d1f2be92babb2c8cb190f1b74e7a32
Ronan Vlak <ronanvlak@gmail.com>
2024-10-15 13:43:16 +0000

    Created FSConverter

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-
-#include "test.h"
+#include "FSConverter.h"
 
 int main() {
-  for (int i = 0; i < 10; i++) {
-    std::cout << "Hello, World!" << std::endl;
-  }
+    try {
+        FSConverter converter;
 
-  test();
-  return 0;
+        // Look for a file called "resource1.txt" in the located Resources folder
+        std::string resourcePath = converter.getResourcePath("resource1.txt");
+
+        if (!resourcePath.empty()) {
+            std::cout << "Full path to resource: " << resourcePath << std::endl;
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return 0;
 }
