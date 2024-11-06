e46b798bf6ab465d961a68fad950374e8e9acf69
Siempie30 <siemvermeulen@hotmail.com>
2024-11-06 13:39:59 +0100

    Start work on gcov

--- code/run_tests.sh
+++ code/run_tests.sh
@@ -2,4 +2,19 @@
 
 cd build
 
+# Run the tests
 bin/Engine_BRAVO_tests
+
+# Create an output directory for the coverage reports
+mkdir -p coverage_reports
+
+cd ..
+
+# Generate the coverage report (ignoring external and build directories)
+gcovr -e external -e build --html --html-details -o build/coverage_reports/coverage.html --delete --txt build/coverage_reports/coverage.txt
+
+# Print the coverage txt summary
+cat build/coverage_reports/coverage.txt
+
+# Delete the coverage note files (coverage data files are already deleted by --delete previous line)
+find . -name "*.gcno" -type f -delete
