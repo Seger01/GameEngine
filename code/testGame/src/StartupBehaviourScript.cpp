32d604541ac4a5d2a6b3596b643024f18d40c8f2
Sean Groenenboom <heavydragon99@outlook.com>
2024-11-14 14:03:57 +0100

    Added splashscreen

---
+++ code/testGame/src/StartupBehaviourScript.cpp
@@ -0,0 +1,32 @@
+#include "StartupBehaviourScript.h"
+
+#include "Engine/EngineBravo.h"
+#include "NetworkBehaviourScript.h"
+
+void StartupBehaviourScript::onStart() { mStartTime = std::chrono::system_clock::now(); }
+
+void StartupBehaviourScript::onUpdate() {
+    auto currentTime = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - mStartTime).count();
+
+    if (duration > 5000) {
+        EngineBravo& engine = EngineBravo::getInstance();
+        SceneManager& sceneManager = engine.getSceneManager();
+        Scene* scene = sceneManager.createScene("networkScene");
+
+        int cameraIDNetwork = scene->addCamera();
+        scene->setActiveCamera(cameraIDNetwork);
+
+        scene->getActiveCamera().setTransform(Transform(Vector2(100, 100)));
+        scene->getActiveCamera().setWidth(200);
+        scene->getActiveCamera().setHeight(200);
+
+        GameObject* gameObject = new GameObject;
+
+        gameObject->addComponent<NetworkBehaviourScript>();
+
+        scene->addGameObject(gameObject);
+
+        sceneManager.requestSceneChange("networkScene");
+    }
+}
