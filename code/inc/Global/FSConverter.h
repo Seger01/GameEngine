6df06452f2368fe43e166e40cbdf0287ae987df3
Siempie30 <siemvermeulen@hotmail.com>
2024-12-17 12:19:11 +0100

    FSConverter: fixed constructor argument to be const correct

--- code/inc/Global/FSConverter.h
+++ code/inc/Global/FSConverter.h
@@ -5,25 +5,27 @@
 #include <stdexcept>
 #include <string>
 #include <unistd.h>
+
 /**
  * @brief Class to direct a file path to the Resources directory
- * @details 
+ * @details
  * This class is responsible for converting a file path to the Resources directory.
  * The main method to perform the conversion is getResourcePath().
  * Once the constructor for the class is called, the Resources directory is cached, and wont have to be found again.
  * @param aResourceDir An optional string containing the path to the Resources directory.
  */
-class FSConverter {
+class FSConverter
+{
 public:
-    FSConverter(std::string aResourceDir = "");
+	FSConverter(const std::string& aResourceDir = "");
 
-    std::string getResourcePath(const std::string& resourceName, bool aCheckExists = false) const;
-
-private:
-    std::string findResourcesFolder() const;
-    std::string executablePath() const;
+	std::string getResourcePath(const std::string& resourceName, bool aCheckExists = false) const;
 
 private:
-    std::string mResourceDir;
-    static std::string mCachedResourceDir;
+	std::string findResourcesFolder() const;
+	std::string executablePath() const;
+
+private:
+	std::string mResourceDir;
+	static std::string mCachedResourceDir;
 };
