afaf89b68b7f43cb89ec5c128feb57bd7ac5de81
Siempie30 <siemvermeulen@hotmail.com>
2024-12-19 15:06:28 +0100

    Added Doxygen to Engine header

--- code/inc/Engine/EngineBravo.h
+++ code/inc/Engine/EngineBravo.h
@@ -1,4 +1,9 @@
 /**
+ * @file EngineBravo.h
+ *
+ * @brief Header file for the EngineBravo class.
+ */
+/**
  * @mainpage Engine Bravo
  *
  * Engine Bravo is the game engine, created for the Systems Programming in C++ minor at Avans University of Applied
@@ -6,8 +11,7 @@
  *
  */
 
-#ifndef ENGINEBRAVO_H
-#define ENGINEBRAVO_H
+#pragma once
 
 #include "AudioManager.h"
 #include "Engine/ResourceManager.h"
@@ -24,10 +28,15 @@
 #include "UpdateQueue.h"
 #include <queue>
 
+/**
+ * @class EngineBravo
+ *
+ * @brief The main class of the engine. It is a singleton and contains all the managers, which are responsible for the
+ * various systems in the engine.
+ */
 class EngineBravo
 {
 public:
-	// Get the single instance of EngineBravo
 	static EngineBravo& getInstance();
 
 	void initialize();
@@ -66,21 +75,32 @@ private:
 	void handleEvent(const Event& aEvent);
 
 private:
+	/// @brief Maximum frame rate (in frames per second)
 	int mFrameRateLimit;
+	/// @brief Flag to indicate if the engine is running. Is used to stop the engine.
 	bool mRunning;
 
+	/// @brief Scene manager: responsible for scene transitions
 	SceneManager mSceneManager;
+	/// @brief Render system: responsible for rendering objects
 	RenderSystem mRenderSystem;
+	/// @brief Resource manager: responsible for loading and managing resources
 	ResourceManager mResourceManager;
+	/// @brief Particle system: responsible for creating and destroying particles
 	ParticleSystem mParticleSystem;
+	/// @brief Network manager: responsible for network communication
 	NetworkManager mNetworkManager;
+	/// @brief Event manager: responsible for handling user input events
 	EventManager mEventManager;
+	/// @brief Save game manager: responsible for saving and loading save games
 	SaveGameManager mSaveGameManager;
+	/// @brief Audio manager: responsible for playing audio
 	AudioManager mAudioManager;
+	/// @brief UI manager: responsible for managing UI elements, such as buttons and HUD
 	UIManager mUIManager;
+	/// @brief Physics manager: responsible for physics calculations
 	PhysicsManager mPhysicsManager;
 
+	/// @brief Update queue: responsible for updating the objects in all managers.
 	UpdateQueue mUpdateQueue;
 };
-
-#endif // ENGINEBRAVO_H
